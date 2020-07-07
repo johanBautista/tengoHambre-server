03deab5ea079eab23f035250dac6f160a71ebe7e
johan Bautista <jsbparra@gmail.com>
2020-07-07 17:15:08 +0200

    config server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log('servidor index.js == ok');
+const express = require('express');
 
+//crear el serer
+const app = express();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//arrancar la app
+app.listen(PORT, () => {
+  console.log(`el servidor funcionaen ${PORT}`);
+});
