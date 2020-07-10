e7924a97dc6563e62aee0917f388b8f03f4bc781
johan Bautista <jsbparra@gmail.com>
2020-07-10 18:00:37 +0200

    crear-guardar usuario y hashear password

--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -1,4 +1,28 @@
-exports.crearUsuario = (req, res) => {
-  console.log('crear usuario desde controller');
-  console.log(req.body);
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
+exports.crearUsuario = async (req, res) => {
+  //extraer password
+  const { nombre, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ nombre });
+    if (usuario) {
+      return res.status(400).json({ msg: ' El usuario ya existe' });
+    }
+    //crear nuevo usuario
+    usuario = new Usuario(req.body);
+
+    //hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+
+    //guardar nuevo usuario
+    await usuario.save();
+    // mensaje confirmacion
+    res.json({ msg: 'Usuario creado correctamente' });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('hubo un error al crear usuario');
+  }
 };
--- models/Usuario.js
+++ models/Usuario.js
@@ -5,6 +5,7 @@ const UsuarioSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    unique: true,
   },
   password: {
     type: String,
